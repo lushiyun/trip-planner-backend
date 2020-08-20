eb21dc95df1a16cd8aa78646704a0a42d6ae8b26
github username <lushiyun@outlook.com>
2020-08-19 23:56:07 -0400

    Complete trip create action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,28 @@
 class TripsController < ApplicationController
 
+  def index
+    trips = Trip.all
+    options = { include: [:days, :'day.places'] }
+    render json: TripSerializer.new(trips, options).serialized_json
+  end
+
+  def create
+    trip = Trip.new(trip_params)
+    if trip.save
+      options = { include: [:days, :'day.places'] }
+      render json: TripSerializer.new(trip, options).serialized_json
+    else
+      render json: { error: 'could not be created' }
+    end
+  end
+
   private
 
   def trip_params
     params.require(:trip).permit(:city, :lat, :lng,
-      :days_attributes => [:date, 
-        :places_attributes => [:name, :place_id, :category]])
+      days_attributes: 
+        [:date, 
+        places_attributes: [:name, :place_id, :category]
+        ])
   end
 end
